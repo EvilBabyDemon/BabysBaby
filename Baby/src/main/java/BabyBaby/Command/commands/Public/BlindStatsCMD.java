962b1bb5e65ef879988ab7947efc6ecf81e39cfe
EvilBabyDemon <lteufelbe@ethz.ch>
2021-06-18 17:59:39 +0200

    added cmd to get Blind Stats back

---
+++ Baby/src/main/java/BabyBaby/Command/commands/Public/BlindStatsCMD.java
@@ -0,0 +1,124 @@
+package BabyBaby.Command.commands.Public;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import BabyBaby.Command.CommandContext;
+import BabyBaby.Command.PublicCMD;
+import BabyBaby.Command.StandardHelp;
+import BabyBaby.data.Data;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+public class BlindStatsCMD implements PublicCMD {
+
+    @Override
+    public String getName() {
+        return "stats";
+    }
+
+    @Override
+    public void handlePublic(CommandContext ctx) {
+
+        String arg = "";
+        
+        if(ctx.getArgs().size()>0)
+            arg = ctx.getArgs().get(0);
+
+        switch(arg){
+            case "":
+                userStats(ctx);
+            break;
+            case "lb": case "leaderboard":
+                leaderboard(ctx);
+            break;
+        }
+
+
+
+    }   
+
+    @Override
+    public MessageEmbed getPublicHelp(String prefix) {
+        return StandardHelp.Help(prefix, getName(), "[lb]", "Get the stats of how many accumulated minutes you were blinded or the leaderboard of this server.");
+    }
+
+    private void userStats(CommandContext ctx) {
+        long rank = 0;
+        Connection c = null;
+        PreparedStatement pstmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(Data.db);
+            pstmt = c.prepareStatement("SELECT RANK FROM STATS WHERE ID=?;");
+            pstmt.setString(1, ctx.getMember().getId());
+
+            ResultSet rs = pstmt.executeQuery();
+            try {
+                rank = rs.getLong("RANK");    
+            } catch (Exception e) {
+                System.out.println("This user has no stats.");
+                e.printStackTrace();
+            }
+            
+            pstmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        ctx.getChannel().sendMessage("Your rank is: " + (rank/60000)).queue();
+    }
+
+    private void leaderboard(CommandContext ctx){
+        ArrayList<Object[]> users = new ArrayList<>();
+        Connection c = null;
+        PreparedStatement pstmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(Data.db);
+            pstmt = c.prepareStatement("SELECT * FROM STATS WHERE RANK=NULL;");
+            
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                String id = rs.getString("ID");
+                long rank = rs.getLong("RANK");
+                users.add(new Object[]{id, rank});
+            }
+
+            pstmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+        Comparator<Object[]> objComp = new Comparator<Object[]>(){
+            @Override
+            public int compare(Object[] o1, Object[] o2) {
+                return (int) ((long) o1[1] - (long) o2[1]);
+            }  
+        };
+
+        users.sort(objComp);
+
+        String lb = "";
+        Guild guild = ctx.getGuild();
+        for (int i = 0; i < 10 && 0 < users.size(); i++) {
+            Object[] obj = users.remove(0);
+            lb += guild.getMemberById(obj[0]+"").getAsMention() + " : " + ((long) obj[0]/60000);
+        }
+
+        EmbedBuilder eb = new EmbedBuilder();
+        
+        eb.setTitle("Leaderboard for blind and forceblind");
+        eb.setDescription(lb);
+    
+        ctx.getChannel().sendMessage(eb.build()).queue();
+    }
+    
+}
