27366e82225daa2371c392b04a8357296f4e05a7
EvilBabyDemon <lteufelbe@ethz.ch>
2021-06-19 00:02:55 +0200

    swapped the comparator

--- Baby/src/main/java/BabyBaby/Command/commands/Public/BlindStatsCMD.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Public/BlindStatsCMD.java
@@ -99,11 +99,12 @@ public class BlindStatsCMD implements PublicCMD {
         Comparator<Object[]> objComp = new Comparator<Object[]>(){
             @Override
             public int compare(Object[] o1, Object[] o2) {
-                return (int) ((long) o1[1] - (long) o2[1]);
+                return (int) ((long) o2[1] - (long) o1[1]);
             }  
         };
 
         users.sort(objComp);
+        
 
         String lb = "";
         Guild guild = ctx.getGuild();
