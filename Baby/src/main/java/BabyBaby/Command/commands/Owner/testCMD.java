c610b4b1f5a7948fdd2164acc91a21418d6f54b6
EvilBabyDemon <lteufelbe@ethz.ch>
2021-06-20 16:40:16 +0200

    testcmd to put all users with student role into db

--- Baby/src/main/java/BabyBaby/Command/commands/Owner/testCMD.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Owner/testCMD.java
@@ -1,11 +1,8 @@
 package BabyBaby.Command.commands.Owner;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
 import java.util.LinkedList;
-import java.util.concurrent.TimeUnit;
 
 /*
 import java.io.File;
@@ -21,11 +18,9 @@ import BabyBaby.Command.CommandContext;
 import BabyBaby.Command.OwnerCMD;
 import BabyBaby.Command.StandardHelp;
 import BabyBaby.data.Data;
-import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.Button;
-import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import net.dv8tion.jda.api.entities.Role;
 
 public class TestCMD implements OwnerCMD{
 
@@ -36,64 +31,33 @@ public class TestCMD implements OwnerCMD{
 
     @Override
     public void handleOwner(CommandContext ctx) {
-        ArrayList<Button> buttons = new ArrayList<>();
-        ArrayList<String> roles = new ArrayList<>();
+        
+        LinkedList<Member> allmem = new LinkedList<>(); 
+        
+        Role student = ctx.getGuild().getRoleById(Data.ethstudent);
+        for (Member mem : ctx.getGuild().getMembers()) {   
+            if(mem.getRoles().contains(student))
+                allmem.add(mem);
+        }
+
         Connection c = null;
-        Statement stmt = null;
+        PreparedStatement pstmt = null;
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection(Data.db);
-            
-            stmt = c.createStatement();
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ASSIGNROLES;");
-            while ( rs.next() ) {
-                roles.add(ctx.getGuild().getRoleById(rs.getString("ID")).getName());
+            for (Member mem : allmem) {
+                pstmt = c.prepareStatement("INSERT OR IGNORE INTO VERIFIED (ID) VALUES (?);");
+                pstmt.setString(1, mem.getId());
+                pstmt.execute();    
+                pstmt.close();
             }
-            rs.close();
-            stmt.close();
+            
             c.close();
         } catch ( Exception e ) {
-            ctx.getChannel().sendMessage(e.getClass().getName() + ": " + e.getMessage()).queue();
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
             return;
         }
-        boolean switcher = true;
-        for (int i = 0; i < 25; i++) {
-            buttons.add(switcher ? Button.success("role", roles.remove((int) (Math.random()*roles.size()))) : Button.danger("role", roles.remove((int) (Math.random()*roles.size()))));
-            switcher = !switcher;
-        }
-        
-        MessageAction adder = ctx.getChannel().sendMessage("Click the buttons :");
-        LinkedList<ActionRow> actionRows = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            actionRows.add(ActionRow.of(buttons.remove(0), buttons.remove(0), buttons.remove(0), buttons.remove(0), buttons.remove(0)));
-        }
-        adder.setActionRows(actionRows);
-        
-        
-
-        Message msg = adder.complete();
-        Data.buttonid.add(msg.getId());
-        msg.editMessage("newContent").setActionRow(Button.success("role", "yay")).completeAfter(10, TimeUnit.SECONDS);
-
-
-        /*
-        ArrayList<Button> buttons = new ArrayList<>();
-        ArrayList<Emote> allemo = new ArrayList<>(ctx.getGuild().getEmotes());
-        for (int i = 0; i < 25; i++) {
-            Emote emo = allemo.remove((int) (Math.random()*allemo.size()));
-            Button tmp = Button.primary(emo.getId(), Emoji.fromEmote(emo));
-            //tmp = tmp.withEmoji(Emoji.fromEmote(emo));
-            buttons.add(tmp);
-        }
-        MessageAction adder = ctx.getChannel().sendMessage("Click the buttons :");
-        LinkedList<ActionRow> actionRows = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            actionRows.add(ActionRow.of(buttons.remove(0), buttons.remove(0), buttons.remove(0), buttons.remove(0), buttons.remove(0)));
-        }
-        adder.setActionRows(actionRows);
-        data.buttonid.add(adder.complete().getId());
-        */
+        ctx.getMessage().reply("Done!").complete();
     }   
 
     @Override
