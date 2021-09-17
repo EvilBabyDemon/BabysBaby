483782fe33d80592114850e59db3e99c7c948b41
EvilBabyDemon <lteufelbe@ethz.ch>
2021-09-18 01:32:12 +0200

    dinkdonk for reminder image. Thank andri

--- Baby/src/main/java/BabyBaby/Command/commands/Public/GetReminder.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Public/GetReminder.java
@@ -3,9 +3,11 @@ package BabyBaby.Command.commands.Public;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.List;
 
 import BabyBaby.data.Data;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 
@@ -43,7 +45,11 @@ public class GetReminder implements Runnable {
             return;
         }
         
+        List<Emote> emoList = guild.getJDA().getEmotesByName("dinkdonk", true);
+
+
         EmbedBuilder eb = new EmbedBuilder();
+        eb.setThumbnail(emoList.get(0).getImageUrl());
         eb.setTitle("Reminder!");
         eb.setColor(0);
         eb.setDescription(text);
