fbb66e2b2288c70eb5e2c6ddf9b4a3523361e6a1
EvilBabyDemon <lteufelbe@ethz.ch>
2021-10-31 09:40:13 +0100

    added cache refresh and multiple invitors possible now

--- Baby/src/main/java/BabyBaby/Command/commands/Admin/WhoisCMD.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Admin/WhoisCMD.java
@@ -29,117 +29,124 @@ public class WhoisCMD implements AdminCMD{
     @Override
     public void handleAdmin(CommandContext ctx) {
         List<String> cmds = ctx.getArgs();
-			Member stalking; 
-			MessageChannel channel = ctx.getChannel();
+		Member stalking; 
+		MessageChannel channel = ctx.getChannel();
 
-			if(cmds.size() >= 1){
-                String person = cmds.get(0);
-				person = person.replaceAll("<", "");
-				person = person.replaceAll(">", "");
-				person = person.replaceAll("!", "");
-				person = person.replaceAll("@", "");
-				stalking = ctx.getGuild().getMemberById(person);
-			} else{
-				stalking = ctx.getMember();
-			}
+		if(cmds.size() >= 1){
+			String person = cmds.get(0);
+			person = person.replaceAll("<", "");
+			person = person.replaceAll(">", "");
+			person = person.replaceAll("!", "");
+			person = person.replaceAll("@", "");
+			stalking = ctx.getGuild().getMemberById(person);
+		} else{
+			stalking = ctx.getMember();
+		}
 
-			
-
-			String nickname = (ctx.getMember().getNickname() != null) ? ctx.getMember().getNickname()
-						: ctx.getMember().getEffectiveName();
-			List<Role> allrolesList = stalking.getRoles();
-			
-			LinkedList<Role> allroles = new LinkedList<>();
-			
-			for (Role role : allrolesList) {
-				allroles.add(role);
-			}
-			allroles.add(ctx.getGuild().getRoleById(ctx.getGuild().getId()));
-			Role highest = allroles.peek();
-			Role hoisted = null;
-			
-			for (Role role : allroles) {
-				if(role.isHoisted()){
-					hoisted = role;
-					break;
-				}
-			}
 		
-			DateTimeFormatter jointime = DateTimeFormatter.ofPattern("E, dd.MM.yyyy, HH:mm");
-			DateTimeFormatter createtime = DateTimeFormatter.ofPattern("E, dd.MM.yyyy");
+
+		String nickname = (ctx.getMember().getNickname() != null) ? ctx.getMember().getNickname()
+					: ctx.getMember().getEffectiveName();
+		List<Role> allrolesList = stalking.getRoles();
+		
+		LinkedList<Role> allroles = new LinkedList<>();
+		
+		for (Role role : allrolesList) {
+			allroles.add(role);
+		}
+		allroles.add(ctx.getGuild().getRoleById(ctx.getGuild().getId()));
+		Role highest = allroles.peek();
+		Role hoisted = null;
+		
+		for (Role role : allroles) {
+			if(role.isHoisted()){
+				hoisted = role;
+				break;
+			}
+		}
+	
+		DateTimeFormatter jointime = DateTimeFormatter.ofPattern("E, dd.MM.yyyy, HH:mm");
+		DateTimeFormatter createtime = DateTimeFormatter.ofPattern("E, dd.MM.yyyy");
+		
+		String rolementions = "";
+		
+		while(allroles != null && rolementions.length() < 250 && allroles.size() != 0){
+			rolementions += allroles.poll().getAsMention() + ", ";
+		}
+		
+		if(allroles.size() == 0){
+			if(rolementions.charAt(rolementions.length()-1) == ','){
+				rolementions = rolementions.substring(0, rolementions.length()-1);
+			} 
+		} else {
+			rolementions += "` and " + allroles.size() + " more...`";
+		}
+		OffsetDateTime created = stalking.getUser().getTimeCreated();
+		OffsetDateTime now = OffsetDateTime.now();
+		int day = now.getDayOfYear() - created.getDayOfYear();
+		day = (day<0)? 365+day:day;
+		int year =  now.getYear() - created.getYear() + ((now.getDayOfYear()<created.getDayOfYear())?-1:0);
+
+		String multyear = ((year + Math.round(day/365.0)) == 1) ? " year ago" : " years ago";
+		String multday = (day== 1) ? " day ago" : " days ago";
+		String actualtime = (year >0) ?  (year + Math.round(day/365.0)) + multyear : day + multday;
+		
+		String addchecks = "Created as: **a " + ((stalking.getUser().isBot()) ? "bot" : "user") + " account** \n Created at: **" + stalking.getUser().getTimeCreated().format(createtime) + "** `(" + actualtime + ")`"; 
+		
+		LinkedList<String> invID = new LinkedList<>();
+		String invitee = "";
+
+		Connection c = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection(Data.db);
 			
-			String rolementions = "";
+			pstmt = c.prepareStatement("SELECT INVITEE FROM INVITED WHERE INVITED = ?;");
+			pstmt.setString(1, stalking.getId());	
+			ResultSet rs = pstmt.executeQuery();
 			
-			while(allroles != null && rolementions.length() < 250 && allroles.size() != 0){
-				rolementions += allroles.poll().getAsMention() + ", ";
+			while(rs.next()){
+				invID.add(rs.getString("INVITEE"));
 			}
 			
-			if(allroles.size() == 0){
-				if(rolementions.charAt(rolementions.length()-1) == ','){
-					rolementions = rolementions.substring(0, rolementions.length()-1);
-				} 
-			} else {
-				rolementions += "` and " + allroles.size() + " more...`";
-			}
-			OffsetDateTime created = stalking.getUser().getTimeCreated();
-			OffsetDateTime now = OffsetDateTime.now();
-			int day = now.getDayOfYear() - created.getDayOfYear();
-			day = (day<0)? 365+day:day;
-			int year =  now.getYear() - created.getYear() + ((now.getDayOfYear()<created.getDayOfYear())?-1:0);
-
-			String multyear = ((year + Math.round(day/365.0)) == 1) ? " year ago" : " years ago";
-			String multday = (day== 1) ? " day ago" : " days ago";
-			String actualtime = (year >0) ?  (year + Math.round(day/365.0)) + multyear : day + multday;
-			
-			String addchecks = "Created as: **a " + ((stalking.getUser().isBot()) ? "bot" : "user") + " account** \n Created at: **" + stalking.getUser().getTimeCreated().format(createtime) + "** `(" + actualtime + ")`"; 
-			
-			String invitee = "NaN";
-
-			Connection c = null;
-			PreparedStatement pstmt = null;
+			pstmt.close();
+			c.close();
+		} catch ( Exception e ) {	
+			System.out.println(e.getClass().getName() + ": " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		if(invID.size()==0){
+			invitee = "NaN";
+		}
+		for (String userID : invID) {
 			try {
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection(Data.db);
-				
-				pstmt = c.prepareStatement("SELECT INVITEE FROM INVITED WHERE INVITED = ?;");
-				pstmt.setString(1, stalking.getId());	
-				ResultSet rs = pstmt.executeQuery();
-				
-				if(rs.next()){
-					invitee = rs.getString("INVITEE");
-				}
-				
-				pstmt.close();
-				c.close();
-			} catch ( Exception e ) {	
-				System.out.println(e.getClass().getName() + ": " + e.getMessage());
-				e.printStackTrace();
-			}
-			//Not sure atm if this throws an error when empty
-
-
-			try {
-				invitee = ctx.getGuild().getMemberById(invitee).getAsMention(); 
+				invitee += ctx.getGuild().getMemberById(userID).getAsMention(); 
 			} catch (Exception e) {
 			}
-			
-			EmbedBuilder eb = new EmbedBuilder();
-			eb.setTitle("@" + stalking.getUser().getAsTag() + " (" + stalking.getId() + ")");
-			eb.setColor(highest.getColor());
-			
-			eb.addField("Nickname", "`" + ((stalking.getNickname() != null) ? stalking.getNickname() : stalking.getEffectiveName()) + "` " + stalking.getAsMention(), false);
-			eb.addField("Joined at", "`" + stalking.getTimeJoined().toLocalDateTime().format(jointime) + "`", false);
-			eb.addField("Invited by", invitee, false);
-			eb.addField("Highest Role", highest.getAsMention(), true);
-			eb.addField("Hoisted Role",(hoisted != null) ? hoisted.getAsMention(): "`Unhoisted`", true);
-			eb.addField("Roles obtained (" + (1+allrolesList.size()) + ")" , rolementions, false);
-			eb.addField("Additional Checks", addchecks, false);
-			// eb.addBlankField(false);
-			eb.setFooter("Summoned by: " + nickname, ctx.getAuthor().getAvatarUrl());
-			eb.setThumbnail(stalking.getUser().getAvatarUrl());
-
-			channel.sendMessageEmbeds(eb.build()).queue();
-        
+		}
+		
+		
+		
+		
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setTitle("@" + stalking.getUser().getAsTag() + " (" + stalking.getId() + ")");
+		eb.setColor(highest.getColor());
+		
+		eb.addField("Nickname", "`" + ((stalking.getNickname() != null) ? stalking.getNickname() : stalking.getEffectiveName()) + "` " + stalking.getAsMention(), false);
+		eb.addField("Joined at", "`" + stalking.getTimeJoined().toLocalDateTime().format(jointime) + "`", false);
+		eb.addField("Invited by", invitee, false);
+		eb.addField("Highest Role", highest.getAsMention(), true);
+		eb.addField("Hoisted Role",(hoisted != null) ? hoisted.getAsMention(): "`Unhoisted`", true);
+		eb.addField("Roles obtained (" + (1+allrolesList.size()) + ")" , rolementions, false);
+		eb.addField("Additional Checks", addchecks, false);
+		eb.setFooter("Summoned by: " + nickname, ctx.getAuthor().getAvatarUrl());
+		eb.setThumbnail(stalking.getUser().getAvatarUrl());
+		
+		if(invID.size()!=0) channel.sendMessage("Cache reload").complete().editMessage(invitee).complete().delete().complete();
+		channel.sendMessageEmbeds(eb.build()).queue();
+		
     }
 
     @Override
