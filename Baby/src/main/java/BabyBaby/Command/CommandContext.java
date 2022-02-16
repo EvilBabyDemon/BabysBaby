4e3caea1d0c7c1c54524466b24177307e225184d
EvilBabyDemon <lteufelbe@ethz.ch>
2022-02-16 09:50:30 +0100

    Implemented all methods of CTX myself

--- Baby/src/main/java/BabyBaby/Command/CommandContext.java
+++ Baby/src/main/java/BabyBaby/Command/CommandContext.java
@@ -1,43 +1,42 @@
 package BabyBaby.Command;
 
-import me.duncte123.botcommons.commands.ICommandContext;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
 
+import BabyBaby.data.Data;
 
-public class CommandContext implements ICommandContext {
 
-    private final GuildMessageReceivedEvent event;
+public class CommandContext {
+
+    private final MessageReceivedEvent event;
     private final List<String> args;
     private final int permissionLevel;
 
-    public CommandContext(GuildMessageReceivedEvent event, List<String> args) {
+    public CommandContext(MessageReceivedEvent event, List<String> args) {
         this.event = event;
         this.args = args;
         Member member = this.getMember();
 
-        Role mod = event.getGuild().getRoleById("815932497920917514");
+        Role mod = event.getGuild().getRoleById(Data.MODERATOR_ID);
 
         // owner -> 2
         // admin -> 1
         // public -> 0
-        permissionLevel = member.getId().equals("223932775474921472") ? 2 : event.getGuild().getId().equals("747752542741725244") && (member.hasPermission(Permission.ADMINISTRATOR) || member.getRoles().contains(mod)) ? 1 : 0;
+        permissionLevel = member.getId().equals(Data.myselfID) ? 2 : event.getGuild().getId().equals(Data.ETH_ID) && (member.hasPermission(Permission.ADMINISTRATOR) || member.getRoles().contains(mod)) ? 1 : 0;
     }
     
 
 
-    @Override
     public Guild getGuild() {
         return this.getEvent().getGuild();
     }
 
-    @Override
-    public GuildMessageReceivedEvent getEvent() {
+    public MessageReceivedEvent getEvent() {
         return this.event;
     }
 
@@ -48,4 +47,74 @@ public class CommandContext implements ICommandContext {
     public int getPermissionLevel() {
         return this.permissionLevel;
     }
+
+    public net.dv8tion.jda.api.entities.TextChannel getChannel() {
+        return this.event.getTextChannel();
+    }
+      
+    public net.dv8tion.jda.api.entities.Message getMessage() {
+      return this.event.getMessage();
+    }
+    
+    public net.dv8tion.jda.api.entities.User getAuthor() {
+      return this.event.getAuthor();
+    }
+    
+    public net.dv8tion.jda.api.entities.Member getMember() {
+      return this.event.getMember();
+    }
+    
+    public net.dv8tion.jda.api.JDA getJDA() {
+      return this.event.getJDA();
+    }
+    
+    public net.dv8tion.jda.api.entities.User getSelfUser() {
+      return this.event.getJDA().getSelfUser();
+    }
+    
+    
+    public net.dv8tion.jda.api.entities.Member getSelfMember() {
+      return this.event.getGuild().getMember(this.getSelfUser());
+    }
+      
 }
+    /*
+   public net.dv8tion.jda.api.entities.Guild getGuild() {
+     return null;
+   }
+   
+   public abstract net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent getEvent();
+   
+   public net.dv8tion.jda.api.entities.TextChannel getChannel() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.entities.Message getMessage() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.entities.User getAuthor() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.entities.Member getMember() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.JDA getJDA() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.sharding.ShardManager getShardManager() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.entities.User getSelfUser() {
+     return null;
+   }
+   
+   public net.dv8tion.jda.api.entities.Member getSelfMember() {
+     return null;
+   }
+ }
+ */
