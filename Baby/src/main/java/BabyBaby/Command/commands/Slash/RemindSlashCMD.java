2c6d9508887d917e261abf451a0be193515d19b4
EvilBabyDemon <lteufelbe@ethz.ch>
2022-02-19 10:29:18 +0100

    finished remind cmd

--- Baby/src/main/java/BabyBaby/Command/commands/Slash/RemindSlashCMD.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Slash/RemindSlashCMD.java
@@ -1,5 +1,17 @@
 package BabyBaby.Command.commands.Slash;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import BabyBaby.Command.commands.Public.GetReminder;
+import BabyBaby.data.Data;
+import BabyBaby.data.Helper;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import BabyBaby.Command.ISlashCMD;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -16,8 +28,83 @@ public class RemindSlashCMD implements ISlashCMD {
 
     @Override
     public void handle(SlashCommandInteractionEvent event, InteractionHook hook, boolean failed) {
-        // TODO Auto-generated method stub
         
+        /*
+        CREATE TABLE "REMINDERS" (
+        "USERID"	TEXT,
+        "TEXTS"	TEXT,
+        "GUILDID"	TEXT,
+        "CHANNELID"	TEXT,
+        "TIME"	TEXT
+        );
+
+        */
+
+        MessageChannel channel = event.getChannel();
+
+
+        String unit = event.getOption("unit") != null ? event.getOption("unit").getAsString() : "";
+		
+        double time = event.getOption("time").getAsDouble();
+        
+        if(time <= 0){
+            Helper.unhook("Use positive numbers thx!", failed, hook, event.getUser());
+            return;
+        }			
+
+        Object[] retrieverObj = Helper.getUnits(unit, time);
+        String strUnit = ""+retrieverObj[0];
+        long rounder = (long) retrieverObj[1];
+        
+
+        if(rounder <= 0){
+            channel.sendMessage("Use numbers above 0 second pls!").queue();
+            return;
+        }
+
+        String message = event.getOption("text") != null ? event.getOption("text").getAsString() :
+                         "Just a Reminder. Didn't specify about what.";
+        String pk = "";
+        long timesql = System.currentTimeMillis() + rounder*1000;
+
+        Connection c = null;
+        PreparedStatement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(Data.db);
+            
+            stmt = c.prepareStatement("INSERT INTO REMINDERS (USERID, TEXTS, GUILDID, CHANNELID, TIME) VALUES (?, ?, ?, ?, ?);");
+            //SELECT last_insert_rowid() FROM REMINDERS;
+            //SELECT MAX(_ROWID_) FROM <table> LIMIT 1;
+
+            stmt.setString(1, event.getUser().getId());
+            stmt.setString(2, message);
+            stmt.setString(3, event.getGuild().getId());
+            stmt.setString(4, event.getChannel().getId());
+            stmt.setString(5, "" + timesql);
+
+            stmt.execute();
+
+            stmt = c.prepareStatement("SELECT MAX(_ROWID_) FROM REMINDERS LIMIT 1;");
+            
+            ResultSet rs = stmt.executeQuery();
+
+            pk = rs.getString("MAX(_ROWID_)");
+            
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            Helper.unhook(e.getClass().getName() + ": " + e.getMessage(), failed, hook, event.getUser());
+            return;
+        }
+        if(pk.equals("")){
+            Helper.unhook("Well I fucked up.... Couldnt get the pk.", failed, hook, event.getUser());
+            return;
+        }
+        ScheduledExecutorService remind = Executors.newScheduledThreadPool(1);
+        remind.schedule(new GetReminder(event.getUser(), event.getGuild(), message, event.getChannel().getId(), pk), rounder , TimeUnit.SECONDS);
+        Helper.unhook(event.getUser().getAsMention() + " You will get Reminded in ~" + time + " " + strUnit + ".", failed, hook, event.getUser());
+
     }
 
     @Override
