fdc873fb4221270c0f37fc386145d9cb6610a700
h4ndr <i9$..#$m1c,[2pmebsz{vla7tfwpcwx69^t8)8]n{;d)zvzkn?%]gf]oztjd@>
2022-02-27 17:42:16 +0100

    making the command publicly usable by restricting privileged information

--- Baby/src/main/java/BabyBaby/Command/commands/Admin/AdminCMDs.java
+++ Baby/src/main/java/BabyBaby/Command/commands/Admin/AdminCMDs.java
@@ -696,7 +696,7 @@ public class AdminCMDs {
 		
 		eb.addField("Nickname", "`" + ((stalking.getNickname() != null) ? stalking.getNickname() : stalking.getEffectiveName()) + "` " + stalking.getAsMention(), false);
 		eb.addField("Joined at", "`" + stalking.getTimeJoined().toLocalDateTime().format(jointime) + "`", false);
-		eb.addField("Invited by", invitee, false);
+        eb.addField("Invited by", (event.getMember().hasPermission(Permission.MODERATE_MEMBERS) ? invitee : "Not available to plebs"), false);
 		eb.addField("Highest Role", highest.getAsMention(), true);
 		eb.addField("Hoisted Role",(hoisted != null) ? hoisted.getAsMention(): "`Unhoisted`", true);
 		eb.addField("Roles obtained (" + (1+allrolesList.size()) + ")" , rolementions, false);
